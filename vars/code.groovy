2de53b13eca260d753149f49b62f4f6653e3b6a7
sound9597 <167160347+sound9597@users.noreply.github.com>
2024-04-25 20:49:40 +0530

    Update code.groovy

--- vars/code.groovy
+++ vars/code.groovy
@@ -4,28 +4,29 @@ def call (string imagename, string region, string ecrname, string credentialsId)
     stages {
         stage('docker-build') {
             steps {
-                bat "docker build -t soundarya ."
+                bat "docker build -t ${imagename} ."
             }
         }
         stage('Deploy to AWS') {
             environment { 
                 // Define environment variables outside of the script block
-                AWS_DEFAULT_REGION = '$(string.region)'
+                bat "docker build -t ${imagename} ."
+                AWS_DEFAULT_REGION = '$string.region}'
                 ECR_REPO_URL = 'https://us-east-1.console.aws.amazon.com/ecr/private-registry/repositories?region=us-east-1'
-                DOCKER_IMAGE_NAME = '$(imagename)'
+                DOCKER_IMAGE_NAME = '${imagename}'
             }
             steps {
                 // Remove the unnecessary script block here
                 // You can add deployment steps directly
                 withCredentials([[
                     $class: 'AmazonWebServicesCredentialsBinding',
-                    credentialsId: '$(credentialsId)',
+                    credentialsId: '$ {credentialsI})',
                     accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                 ]]) {
                     bat "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 533267263918.dkr.ecr.us-east-1.amazonaws.com"
-                    bat "docker tag soundarya 533267263918.dkr.ecr.us-east-1.amazonaws.com/$(ecrname):latest"
-                    bat "docker push 533267263918.dkr.ecr.us-east-1.amazonaws.com/$(ecrname):latest"
+                    bat "docker tag soundarya 533267263918.dkr.ecr.us-east-1.amazonaws.com/$ {ecrname}:latest"
+                    bat "docker push 533267263918.dkr.ecr.us-east-1.amazonaws.com/$ {ecrname}:latest"
                 }
             }
         }
