63ad3542222e5c5ffbdd1384da1918a3aab84513
sound9597 <167160347+sound9597@users.noreply.github.com>
2024-04-25 19:31:08 +0530

    Update web.groovy

--- vars/web.groovy
+++ vars/web.groovy
@@ -4,32 +4,36 @@ def call(String imagename, String region, String ecrname, String credentialsId) 
         stages {
             stage('docker-build') {
                 steps {
-                    bat "docker build -t ${imagename} ."
+                    script {
+                        // Wrap the bat command in a script block
+                        bat "docker build -t ${imagename} ."
+                    }
                 }
             }
             stage('Deploy to AWS') {
                 environment { 
                     // Define environment variables outside of the script block
                     AWS_DEFAULT_REGION = region
-                    ECR_REPO_URL = "533267263918.dkr.ecr.us-east-1.amazonaws.com/${ecrname}"
+                    ECR_REPO_URL = "533267263918.dkr.ecr.${region}.amazonaws.com/${ecrname}"
                     DOCKER_IMAGE_NAME = imagename
                 }
                 steps {
-                    // Remove the unnecessary script block here
-                    // You can add deployment steps directly
-                    withCredentials([[
-                        $class: 'AmazonWebServicesCredentialsBinding',
-                        credentialsId: credentialsId,
-                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                    ]]) {
-                        bat "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ECR_REPO_URL}"
-                        bat "docker tag ${imagename} ${ECR_REPO_URL}:latest"
-                        bat "docker push ${ECR_REPO_URL}:latest"
+                    // Wrap the steps in a script block
+                    script {
+                        withCredentials([[
+                            $class: 'AmazonWebServicesCredentialsBinding',
+                            credentialsId: credentialsId,
+                            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                        ]]) {
+                            // Wrap bat commands in a script block
+                            bat "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ECR_REPO_URL}"
+                            bat "docker tag ${imagename} ${ECR_REPO_URL}:latest"
+                            bat "docker push ${ECR_REPO_URL}:latest"
+                        }
                     }
                 }
             }
         }
     }
-}
 }
