f304e5f97c63d5419a22b4a6bfd9fa81f01b270d
sound9597 <167160347+sound9597@users.noreply.github.com>
2024-04-25 20:50:55 +0530

    Update code.groovy

--- vars/code.groovy
+++ vars/code.groovy
@@ -25,8 +25,8 @@ def call (string imagename, string region, string ecrname, string credentialsId)
                     secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                 ]]) {
                     bat "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 533267263918.dkr.ecr.us-east-1.amazonaws.com"
-                    bat "docker tag soundarya 533267263918.dkr.ecr.us-east-1.amazonaws.com/$ {ecrname}:latest"
-                    bat "docker push 533267263918.dkr.ecr.us-east-1.amazonaws.com/$ {ecrname}:latest"
+                    bat "docker tag soundarya 533267263918.dkr.ecr.us-east-1.amazonaws.com/${ecrname}:latest"
+                    bat "docker push 533267263918.dkr.ecr.us-east-1.amazonaws.com/${ecrname}:latest"
                 }
             }
         }
