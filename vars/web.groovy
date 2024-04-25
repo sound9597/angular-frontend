cb3bfd4313efab9065b3b9e7799e01d9c8d8e4b6
sound9597 <167160347+sound9597@users.noreply.github.com>
2024-04-25 19:57:22 +0530

    Update web.groovy

--- vars/web.groovy
+++ vars/web.groovy
@@ -1,33 +1,32 @@
-// File: deployToAWS.groovy
-
-def call(String imageName, String region, String ecrName, String credentialsId) {
+def call(String imagename, String region, String ecrname, String credentialsId) {
     pipeline {
         agent any
         stages {
             stage('docker-build') {
                 steps {
-                    bat "docker build -t ${imageName} ."
+                    script {
+                        bat "docker build -t ${imagename} ."
+                    }
                 }
             }
             stage('Deploy to AWS') {
                 environment { 
-                    // Define environment variables outside of the script block
                     AWS_DEFAULT_REGION = region
-                    ECR_REPO_URL = 'https://us-east-1.console.aws.amazon.com/ecr/private-registry/repositories?region=us-east-1'
-                    DOCKER_IMAGE_NAME = imageName
+                    ECR_REPO_URL = "533267263918.dkr.ecr.${region}.amazonaws.com/${ecrname}"
+                    DOCKER_IMAGE_NAME = imagename
                 }
                 steps {
-                    // Remove the unnecessary script block here
-                    // You can add deployment steps directly
                     withCredentials([[
                         $class: 'AmazonWebServicesCredentialsBinding',
                         credentialsId: credentialsId,
                         accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                         secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                     ]]) {
-                        bat "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin 533267263918.dkr.ecr.us-east-1.amazonaws.com"
-                        bat "docker tag ${imageName} 533267263918.dkr.ecr.us-east-1.amazonaws.com/${ecrName}:latest"
-                        bat "docker push 533267263918.dkr.ecr.us-east-1.amazonaws.com/${ecrName}:latest"
+                        script {
+                            bat "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ECR_REPO_URL}"
+                            bat "docker tag ${imagename} ${ECR_REPO_URL}:latest"
+                            bat "docker push ${ECR_REPO_URL}:latest"
+                        }
                     }
                 }
             }
