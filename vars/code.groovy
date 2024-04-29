7c34d1cc5885aff8176f8bff89ca481b960dfbce
sound9597 <167160347+sound9597@users.noreply.github.com>
2024-04-29 08:54:36 +0530

    Update code.groovy

--- vars/code.groovy
+++ vars/code.groovy
@@ -1,31 +1,35 @@
 def call(String imagename, String region, String ecrname, String credentialsId) {
     pipeline {
         agent any
-           stages {
+        stages {
             stage('docker-build') {
                 steps {
                     script {
-                        bat "docker build -t ${imagename} ."
+                        // Docker build stage
+                        sh "docker build -t ${imagename} ."
                     }
                 }
             }
             stage('Deploy to AWS') {
                 environment { 
+                    // Define environment variables
                     AWS_DEFAULT_REGION = "${region}"
                     ECR_REPO_URL = "533267263918.dkr.ecr.${region}.amazonaws.com/${ecrname}"
                     DOCKER_IMAGE_NAME = "${imagename}"
                 }
                 steps {
-                    withCredentials([[
-                        $class: 'AmazonWebServicesCredentialsBinding',
-                        credentialsId: credentialsId,
-                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                    ]]) {
+                    // AWS ECR authentication and Docker push stage
+                    withCredentials([
+                        [ $class: 'AmazonWebServicesCredentialsBinding',
+                          credentialsId: credentialsId,
+                          accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                          secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                        ]
+                    ]) {
                         script {
-                            bat "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ECR_REPO_URL}"
-                            bat "docker tag ${imagename} ${ECR_REPO_URL}:latest"
-                            bat "docker push ${ECR_REPO_URL}:latest"
+                            sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ECR_REPO_URL}"
+                            sh "docker tag ${imagename} ${ECR_REPO_URL}:latest"
+                            sh "docker push ${ECR_REPO_URL}:latest"
                         }
                     }
                 }
@@ -33,5 +37,3 @@ def call(String imagename, String region, String ecrname, String credentialsId) 
         }
     }
 }
-
-
