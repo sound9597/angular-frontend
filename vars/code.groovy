58ff12c79a75d1d38038d5b11da90aa50872989e
sound9597 <167160347+sound9597@users.noreply.github.com>
2024-05-01 13:31:34 +0530

    Update code.groovy

--- vars/code.groovy
+++ vars/code.groovy
@@ -10,14 +10,14 @@ def call(String imagename, String region, String ecrname, String credentialsId) 
                     }
                 }
             }
-            stage('Deploy to AWS') {
+               stage('Deploy to AWS') {
                 environment { 
                     // Define environment variables
                     AWS_DEFAULT_REGION = "${region}"
                     ECR_REPO_URL = "533267263918.dkr.ecr.${region}.amazonaws.com/${ecrname}"
                     DOCKER_IMAGE_NAME = "${imagename}"
                 }
-                steps {
+                       steps {
                     // AWS ECR authentication and Docker push stage
                     withCredentials([
                         [ $class: 'AmazonWebServicesCredentialsBinding',
@@ -26,7 +26,7 @@ def call(String imagename, String region, String ecrname, String credentialsId) 
                           secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
                         ]
                     ]) {
-                        script {
+                         script {
                             sh "aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${ECR_REPO_URL}"
                             sh "docker tag ${imagename} ${ECR_REPO_URL}:latest"
                             sh "docker push ${ECR_REPO_URL}:latest"
